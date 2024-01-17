d116ba6b0bb0b175d5d57547be7439f078ea638d
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-01-17 20:50:11 +0300

    Commit Front End Layout files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/messages', [MessageController::class,'index'])->name('messages.index');
 
+Route::get('/messages', [MessageController::class,'store'])->name('messages.store');
